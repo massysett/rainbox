310f6c7a593d5d4ee3422c91a0afbd47efb0acee
Omari Norman <omari@smileystation.com>
2014-03-04 18:13:19 -0500

    WIP

--- lib/Text/PrettyPrint/Rainbox/Box.hs
+++ lib/Text/PrettyPrint/Rainbox/Box.hs
@@ -54,20 +54,61 @@ chunkBox = Box . (:[]) . Row
 data Alignment a = Center | NonCenter a
   deriving (Eq, Show)
 
-data Horizontal = Top | Bottom
+data Vertical = ATop | ABottom
   deriving (Eq, Show)
 
-data Vertical = Left | Right
+data Horizontal = ALeft | ARight
   deriving (Eq, Show)
 
-hcat :: Background -> Alignment Horizontal -> [Box] -> Box
+-- | Merge several Box horizontally into one Box.  That is, with
+-- alignment set to ATop:
+--
+-- > --- ------- ----
+-- > --- -------
+-- > ---
+--
+-- becomes
+--
+-- > --------------
+-- > ----------....
+-- > ---...........
+
+hcat :: Background -> Alignment Vertical -> [Box] -> Box
 hcat bk al bs = Box . mergeHoriz . map (pad . unBox) $ bs
   where
-    pad = padHoriz al height
+    pad = padHoriz bk al height
     height = F.maximum . (Rows 0:) . map rows $ bs
 
+-- | Merge several Box vertically into one Box.  That is, with
+-- alignment set to ALeft:
+--
+-- > -------
+-- > -------
+-- >
+-- > ---
+-- > ---
+--
+-- > ----
+-- > ----
+--
+-- becomes
+--
+-- > -------
+-- > -------
+-- > ---....
+-- > ---....
+-- > ---....
+-- > ----...
+-- > ----...
+
+vcat :: Background -> Alignment Horizontal -> [Box] -> Box
+vcat bk al bs = Box . map (padVert bk al w) . concat . map unBox $ bs
+  where
+    w = F.maximum . (Cols 0:) . map cols $ bs
+
+
 -- | Given the resulting height, pad a list of Rows.  So, when given
--- a height of 3 and an alignment of Top,
+-- a height of 3 and an alignment of ATop,
 --
 -- > --------
 -- > --------
@@ -80,9 +121,43 @@ hcat bk al bs = Box . mergeHoriz . map (pad . unBox) $ bs
 --
 -- where dashes is a Row with data, and dots is a blank Row.
 
-padHoriz :: Alignment Horizontal -> Rows -> [Row] -> [Row]
-padHoriz = undefined
+padHoriz :: Background -> Alignment Vertical -> Rows -> [Row] -> [Row]
+padHoriz bk a (Rows tgt) rs = concat [top, rs, bot]
+  where
+    nPad = max 0 $ tgt - length rs
+    (nATop, nBot) = case a of
+      Center -> split nPad
+      NonCenter ATop -> (0, nPad)
+      NonCenter ABottom -> (nPad, 0)
+    pad = Row [blanks bk len]
+      where
+        len = case rs of
+          [] -> Cols 0
+          x:_ -> cols x
+    (top, bot) = (replicate nATop pad, replicate nBot pad)
 
+-- | Given the resulting width, pad a Row.  So, when given
+-- a width of 10 and an alignment of ARight,
+--
+-- > -------
+--
+-- becomes
+--
+-- > ...-------
+
+padVert :: Background -> Alignment Horizontal -> Cols -> Row -> Row
+padVert bk a (Cols tgt) (Row cs) = Row . concat $ [left, cs, right]
+  where
+    nPad = max 0 $ tgt - length cs
+    (nLeft, nRight) = case a of
+      Center -> split nPad
+      NonCenter ALeft -> (0, nPad)
+      NonCenter ARight -> (nPad, 0)
+    (left, right) = (mkPad nLeft, mkPad nRight)
+    mkPad n
+      | n == 0 = []
+      | otherwise = [blanks bk (Cols n)]
+        
 
 -- | Merge several horizontal Rows into one set of horizontal Row.
 -- That is:
@@ -105,8 +180,57 @@ mergeHoriz = foldr (zipWith merge) (repeat (Row []))
   where
     merge (Row r1) (Row r2) = Row $ r1 ++ r2
 
+-- # Viewing
 
+-- | View a Box, possibly shrinking it.  You set the size of your
+-- viewport and how it is oriented relative to the Box as a whole.
+-- The Box returned may be smaller than the argument Box, but it
+-- will never be bigger.
 
+view
+  :: Rows
+  -> Cols
+  -> Alignment Horizontal
+  -> Alignment Vertical
+  -> Box
+  -> Box
+view r c ah av (Box inRows) = Box . map htrim . vtrim $ inRows
+  where
+    vtrim = case av of
+      NonCenter ATop -> take . unRows $ r
+      NonCenter ABottom -> drop extra
+      Center -> drop nDrop . take nTake
+        where
+          (trimL, trimR) = split extra
+          nTake = length inRows - trimR
+          nDrop = trimL
+      where
+        extra = length inRows - unRows r
+    htrim rw = undefined
+
+dropChars :: Int -> Row -> Row
+dropChars n (Row cs)
+  | n <= 0 = Row cs
+  | otherwise = case cs of
+      [] -> Row []
+      x:xs
+        | lenX <= n -> dropChars (n - lenX) (Row xs)
+        | otherwise -> Row $ x' : xs
+        where
+          lenX = X.length . text $ x
+          x' = x { text = X.drop n . text $ x }
+
+takeChars :: Int -> Row -> Row
+takeChars n (Row cs)
+  | n <= 0 = Row []
+  | otherwise = case cs of
+      [] -> Row []
+      x:xs
+        | lenX <= n -> Row $ x : unRow (takeChars (n - lenX) (Row xs))
+        | otherwise -> Row [x']
+        where
+          lenX = X.length . text $ x
+          x' = x { text = X.take n . text $ x }
 
 --
 -- # Helpers
@@ -133,88 +257,3 @@ split i = (r, r + rm)
   where
     (r, rm) = i `quotRem` 2
 
-{-
-
--- | The basic data type.  A box has a specified size and some sort of
---   contents.
-data Box = Box
-  { rows :: Int
-  , cols :: Int
-  , background :: Background
-  , content :: Content
-  } deriving Show
-
--- | Contents of a box.
-data Content
-  = Blank Background Int Int
-  -- ^ No content.  Has given number of rows and columns.
-
-  | Chunks [Chunk]
-  -- ^ A raw string.  Has one row and a number of columns equal to
-  -- the number of characters in the component Chunks.
-
-  | Row Background [Box]
-  -- ^ A row of sub-boxes.  Number of rows is equal to the number of
-  -- rows in the tallest Box.  Number of columns is equal to the sum
-  -- of the number of columns in component Box.
-
-  | Col Background [Box]
-  -- ^ A column of sub-boxes.  Number of columns is equal to the
-  -- number of columns 
-  deriving Show
-
--- | The null box, which has no content and no size.  It is quite
---   useless.
-nullBox :: Background -> Box
-nullBox bk = emptyBox bk 0 0
-
--- | @emptyBox r c@ is an empty box with @r@ rows and @c@ columns.
---   Useful for effecting more fine-grained positioning of other
---   boxes, by inserting empty boxes of the desired size in between
---   them.
-emptyBox :: Background -> Int -> Int -> Box
-emptyBox b r c = Box r c b Blank
-
--- | Contains chunks; the box is 1 tall and its length is the sum of
--- the lengths of the chunks.
-chunks :: Background -> [Chunk] -> Box
-chunks b cs = Box 1 (sum . map X.length . map text $ cs) b (Chunks cs)
-
---
--- # Gluing
---
-
--- | Glue a list of boxes together horizontally, with the given alignment.
-hcat :: Background -> Alignment -> [Box] -> Box
-hcat b a bs = Box h w b (Row $ map (alignVert b a h) bs)
-  where h = maximum . (0:) . map rows $ bs
-        w = sum . map cols $ bs
-
-instance Monoid Box where
-  mappend l r = hcat defaultBackground top [l, r]
-  mempty = nullBox defaultBackground
-
-
-
---
--- # Alignment
---
-
--- | @alignHoriz algn n bx@ creates a box of width @n@, with the
---   contents and height of @bx@, horizontally aligned according to
---   @algn@.
-alignHoriz :: Background -> Alignment -> Int -> Box -> Box
-alignHoriz bk a c b = Box (rows b) c bk $ SubBox a AlignFirst b
-
--- | @alignVert algn n bx@ creates a box of height @n@, with the
---   contents and width of @bx@, vertically aligned according to
---   @algn@.
-alignVert :: Background -> Alignment -> Int -> Box -> Box
-alignVert bk a r b = Box r (cols b) bk $ SubBox AlignFirst a b
-
--- | @align ah av r c bx@ creates an @r@ x @c@ box with the contents
---   of @bx@, aligned horizontally according to @ah@ and vertically
---   according to @av@.
-align :: Background -> Alignment -> Alignment -> Int -> Int -> Box -> Box
-align bk ah av r c = Box r c bk . SubBox ah av
--}
