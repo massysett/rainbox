fae24f049669e1b8aacafa1b8dd075164b8f6e31
Omari Norman <omari@smileystation.com>
2014-03-11 13:35:14 -0400

    WIP

--- test/Rainbox/BoxTests.hs
+++ test/Rainbox/BoxTests.hs
@@ -18,14 +18,14 @@ genText = fmap X.pack $ listOf c
 genChunk :: Gen Chunk
 genChunk = genText >>= G.chunk
 
-genRows :: Gen Rows
-genRows = fmap Rows $ frequency [(3, nonNeg), (1, neg)]
+genHeight :: Gen Height
+genHeight = fmap Height $ frequency [(3, nonNeg), (1, neg)]
   where
     nonNeg = fmap getNonNegative arbitrarySizedIntegral
     neg = fmap (negate . getPositive) arbitrarySizedIntegral
 
-genCols :: Gen Cols
-genCols = fmap Cols $ frequency [(3, nonNeg), (1, neg)]
+genWidth :: Gen Width
+genWidth = fmap Width $ frequency [(3, nonNeg), (1, neg)]
   where
     nonNeg = fmap getNonNegative arbitrarySizedIntegral
     neg = fmap (negate . getPositive) arbitrarySizedIntegral
@@ -37,8 +37,8 @@ genBackground = liftM2 Background G.colors8 G.colors256
 genBox :: Gen Box
 genBox = liftM3 blank genBackground rw cl
   where
-    rw = fmap (Rows . getNonNegative) arbitrarySizedIntegral
-    cl = fmap (Cols . getNonNegative) arbitrarySizedIntegral
+    rw = fmap (Height . getNonNegative) arbitrarySizedIntegral
+    cl = fmap (Width . getNonNegative) arbitrarySizedIntegral
 
 -- # Alignment
 
@@ -54,143 +54,106 @@ validBox box = case unBox box of
   NoHeight i -> i > -1
   WithHeight rw -> case rw of
     [] -> False
-    x:xs -> all (== cols x) . map cols $ xs
+    x:xs -> all (== width x) . map width $ xs
 
 biggest :: Int -> Gen a -> Gen a
 biggest m g = sized $ \s -> resize (min s m) g
 
-data BlankInputs = BlankInputs
-  { biBackground :: Background
-  , biRows :: Rows
-  , biCols :: Cols
+data Inputs = Inputs
+  { iChunks :: [Chunk]
+  , iBackground :: Background
+  , iHeight :: Height
+  , iWidth :: Width
+  , iVert :: Align Vert
+  , iHoriz :: Align Horiz
+  , iBoxes :: [Box]
+  , iBox :: Box
   } deriving Show
 
-instance Arbitrary BlankInputs where
-  arbitrary = BlankInputs <$> genBackground <*> genRows <*> genCols
-
-data ChunksInputs = ChunksInputs
-  { ciChunks :: [Chunk] }
-  deriving Show
-
-instance Arbitrary ChunksInputs where
-  arbitrary = ChunksInputs <$> listOf genChunk
-
-data CatHInputs = CatHInputs
-  { hcBackground :: Background
-  , hcAlign :: Align Vert
-  , hcBoxes :: [Box]
-  } deriving Show
-
-instance Arbitrary CatHInputs where
-  arbitrary = liftM3 CatHInputs genBackground genAlignVert
-    (listOf genBox)
-
-data CatVInputs = CatVInputs
-  { vcBackground :: Background
-  , vcAlign :: Align Horiz
-  , vcBoxes :: [Box]
-  } deriving Show
-
-instance Arbitrary CatVInputs where
-  arbitrary = liftM3 CatVInputs genBackground genAlignHoriz
-    (listOf genBox)
-
-data ViewInputs = ViewInputs
-  { viRows :: Rows
-  , viCols :: Cols
-  , viHoriz :: Align Horiz
-  , viVert :: Align Vert
-  , viBox :: Box
-  } deriving Show
-
-instance Arbitrary ViewInputs where
-  arbitrary = liftM5 ViewInputs genRows genCols genAlignHoriz
-    genAlignVert genBox
+instance Arbitrary Inputs where
+  arbitrary = Inputs
+    <$> listOf genChunk
+    <*> genBackground
+    <*> genHeight
+    <*> genWidth
+    <*> genAlignVert
+    <*> genAlignHoriz
+    <*> listOf genBox
+    <*> genBox
 
 tests :: TestTree
 tests = testGroup "BoxTests"
   [ testGroup "blank"
-    [ testProperty "makes valid Box" $
-      \(BlankInputs bk rw cl) ->
-      validBox (blank bk rw cl)
+    [ testProperty "makes valid Box" $ \i ->
+      validBox $ blank (iBackground i) (iHeight i)
+        (iWidth i)
 
-    , testProperty "has right number of rows" $
-      \(BlankInputs bk rw@(Rows n) cl) ->
-      let numRows | n <= 0 = Rows 0
-                  | otherwise = rw
-      in (== numRows) . rows $ blank bk rw cl
+    , testProperty "has right number of rows" $ \i ->
+      let ht = unHeight . iHeight $ i
+      in (== max 0 ht) . height $ blank (iBackground i)
+            (iHeight i) (iWidth i)
 
-    , testProperty "has right number of columns" $
-      \(BlankInputs bk rw@(Rows nr) cl@(Cols nc)) ->
-      let numCols | nr <= 0 = Cols 0
-                  | nc <= 0 = Cols 0
-                  | otherwise = cl
-      in (== numCols) . cols $ blank bk rw cl
+    , testProperty "has right number of columns" $ \i ->
+      let wt = unWidth . iWidth $ i
+      in (== max 0 wt) . width $ blank (iBackground i)
+            (iHeight i) (iWidth i)
     ]
 
-  , testGroup "chunk"
+  , testGroup "chunks"
     [ testProperty "makes valid Box" $
-      validBox . chunks . ciChunks
+      validBox . chunks . iChunks
 
     , testProperty "makes Box whose height is 1" $
-      (== Rows 1) . rows . chunks . ciChunks
+      (== 1) . height . chunks . iChunks
 
-    , testProperty "makes Box with cols == number of characters" $
-      \(ChunksInputs ci) ->
-      let nChars = sum . map X.length . map text $ ci
-      in (== Cols nChars) . cols . chunks $ ci
+    , testProperty "makes Box with cols == number of characters" $ \i ->
+      let cks = iChunks i
+          nChars = sum . map X.length . map text $ cks
+      in (== nChars) . width $ chunks cks
     ]
 
   , testGroup "catH"
-    [ testProperty "makes valid Box" $
-      \(CatHInputs bk a bs) -> validBox $ catH bk a bs
+    [ testProperty "makes valid Box" $ \i ->
+      validBox $ catH (iBackground i) (iVert i) (iBoxes i)
 
-    , testProperty "is as tall as tallest box" $
-      \(CatHInputs bk a bs) ->
-      let h = maximum . (Rows 0 :) . map rows $ bs
-      in (== h) . rows $ catH bk a bs
+    , testProperty "is as tall as tallest box" $ \i ->
+      let h = maximum . (0 :) . map height $ bs
+          bs = iBoxes i
+      in (== h) . height $ catH (iBackground i) (iVert i) bs
 
-    , testProperty "is as wide as sum of all widths" $
-      \(CatHInputs bk a bs) ->
-      let s = sum . map (unCols . cols) $ bs
-      in (== s) . unCols . cols $ catH bk a bs
+    , testProperty "is as wide as sum of all widths" $ \i ->
+      let s = sum . map width $ bs
+          bs = iBoxes i
+      in (== s) . width $ catH (iBackground i) (iVert i) bs
     ]
 
   , testGroup "catV"
-    [ testProperty "makes a valid Box" $
-      \(CatVInputs bk a bs) ->
-      validBox $ catV bk a bs
+    [ testProperty "makes a valid Box" $ \i ->
+      validBox $ catV (iBackground i) (iHoriz i) (iBoxes i)
 
-    , testProperty "is as tall as the sum of all heights" $
-      \(CatVInputs bk a bs) ->
-      let h = sum . map (unRows . rows) $ bs
-      in (== h) . unRows . rows $ catV bk a bs
+    , testProperty "is as tall as the sum of all heights" $ \i ->
+      let h = sum . map height $ bs
+          bs = iBoxes i
+      in (== h) . height $ catV (iBackground i) (iHoriz i) bs
 
-    , testProperty "is as wide as the widest box" $
-      \(CatVInputs bk a bs) ->
-      let w = maximum . (Cols 0:) . map cols $ bs
-      in (== w) . cols $ catV bk a bs
+    , testProperty "is as wide as the widest box" $ \i ->
+      let w = maximum . (0:) . map width $ bs
+          bs = iBoxes i
+      in (== w) . width $ catV (iBackground i) (iHoriz i) bs
     ]
 
-  , testGroup "view"
-    [ testProperty "makes a valid Box" $
-      \(ViewInputs r c h v b) -> validBox $ view r c h v b
+  , testGroup "viewH"
+    [ testProperty "makes a valid Box" $ \i ->
+      validBox $ viewH (unWidth . iWidth $ i) (iHoriz i) (iBox i)
 
-    , testProperty "number of rows does not increase" $
-      \(ViewInputs r c h v b) ->
-      (<= rows b) . rows $ view r c h v b
+    , testProperty "number of rows does not change" $ \i ->
+      let b = iBox i
+      in (== height b) . height $ viewH (unWidth . iWidth $ i)
+                                    (iHoriz i) b
 
-    , testProperty "number of columns does not increase" $
-      \(ViewInputs r c h v b) ->
-      (<= cols b) . cols $ view r c h v b
-
-    , testProperty "number of rows <= number requested" $
-      \(ViewInputs r c h v b) ->
-      (<= max (Rows 0) r) . rows $ view r c h v b
-
-    , testProperty "number of columns <= number requested" $
-      \(ViewInputs r c h v b) ->
-      (<= max (Cols 0) c) . cols $ view r c h v b
+    , testProperty "number of columns <= number requested" $ \i ->
+      let c = unWidth . iWidth $ i
+      in (<= c) . width $ viewH c (iHoriz i) (iBox i)
     ]
   ]
 
