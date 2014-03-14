6ac93832b3f78353b1f28f5614e0c34b99c7daff
Omari Norman <omari@smileystation.com>
2014-03-14 17:18:16 -0400

    WIP

--- lib/Rainbox/Table.hs
+++ lib/Rainbox/Table.hs
@@ -1,19 +1,78 @@
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
 module Rainbox.Table where
 
-import Rainbox
+import Rainbox hiding (Row)
 import System.Console.Rainbow
 
-data Cell a = Cell { unCell :: [a] }
-  deriving (Eq, Show)
+data Crate = Crate
+  { protect :: Bool
+  , crateChunk :: Chunk
+  } deriving Show
 
-data Record col a = Record { unRecord :: [(col, Cell a)] }
-  deriving (Eq, Show)
+data HardLine = HardLine { unHardLine :: [Crate] }
+  deriving Show
 
-data Table row col a = Table { unTable :: [(row, Record col a)] }
-  deriving (Eq, Show)
+data Word = Word { unWord :: [Crate] }
+  deriving Show
+
+data Tagged p t = Tagged
+  { payload :: p
+  , tag :: t
+  } deriving Show
+
+data CellPayload
+  = Hard [HardLine]
+  | Soft [Word]
+  deriving Show
+
+type Cell = Tagged CellPayload
+
+data AllocSpec = AllocSpec
+  { asMinimum :: Int
+  -- ^ Column will always be at least this wide
+  , asRatio :: Int
+  -- ^ Column consumes this much of the table's leftover space
+  } deriving Show
+
+data WidthSpec
+  = Expand
+  | Fixed Int
+  | Alloc AllocSpec
+  deriving Show
+
+data Column cm = Column
+  { colSpec :: WidthSpec
+  , colCells :: [Cell cm]
+  } deriving Show
+
+widths
+  :: Int
+  -- ^ Total desired width of table
+  -> [Column cm]
+  -> [Int]
+  -- ^ Width of each column
+widths = undefined
+
+data Row cm = Row
+  { rowCells :: [Cell cm]
+  } deriving Show
+
+table
+  :: Int
+  -- ^ Total desired width of table
+  -> [WidthSpec]
+  -> [Row cm]
+  -> [Int]
+  -- ^ Width of each column
+table = undefined
 
 formatTable
-  :: (row -> col -> (Background, Align Vert, Align Horiz))
-  -> Table row col Chunk
-  -> Box
+  :: [Tagged (Row cm) rm]
+  -> [Tagged Int clm]
+  -- ^ One for each column in the table.  If there are any rows with
+  -- extra columns, they will be defaulted to Expand, aligned left.
+  -> (rm -> cm -> clm -> (Background, Align Horiz))
+  -- ^ Applied to each cell in table
+  -> [[Box]]
 formatTable = undefined
+
