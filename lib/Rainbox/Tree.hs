e608413342c73c62b2e223f42b63a2532bc99ce9
Omari Norman <omari@smileystation.com>
2014-03-15 14:12:22 -0400

    WIP

--- lib/Rainbox/Tree.hs
+++ lib/Rainbox/Tree.hs
@@ -154,6 +154,8 @@ data Attributes a = Attributes
 
 data Orientation = OHoriz | OVert deriving (Eq, Show)
 
+type Forest a = [Box a]
+
 data Container a = Container
   { orientation :: Orientation
   , children :: Forest a
@@ -169,67 +171,3 @@ instance HasHeight (Container a) where
     OHoriz -> maximum . (0:) . map height . children $ c
     OVert -> sum . map height . children $ c
 
---
--- # Zipper
---
-
-type Forest a = [Box a]
-
-data TreePos t a = Loc
-  { content :: t a
-  , before :: Forest a
-  , after :: Forest a
-  , parents :: [(Forest a, Box a, Forest a)]
-  } deriving (Eq, Show)
-
-class PosType t where
-  prev :: TreePos t a -> Maybe (TreePos t a)
-  next :: TreePos t a -> Maybe (TreePos t a)
-  forest :: TreePos t a -> Forest a
-
-data Empty a = Empty deriving (Eq, Show)
-
-instance PosType Empty where
-  prev = fmap prevSpace . prevTree
-  next = fmap nextSpace . nextTree
-  forest loc = foldl (flip (:)) (after loc) (before loc)
-
-newtype Full a = Full { unFull :: Box a } deriving (Eq, Show)
-
-instance PosType Full where
-  prev = prevTree . prevSpace
-  next = nextTree . nextSpace
-  forest loc = foldl (flip (:)) (tree loc : after loc) (before loc)
-
-prevSpace :: TreePos Full a -> TreePos Empty a
-prevSpace loc = loc { content = Empty, after = tree loc : after loc }
-
-prevTree :: TreePos t a -> Maybe (TreePos Full a)
-prevTree loc = case before loc of
-  t:ts -> Just loc { content = Full t, before = ts }
-  [] -> Nothing
-
-nextSpace :: TreePos Full a -> TreePos Empty a
-nextSpace loc = loc { content = Empty, before = tree loc : before loc }
-
-nextTree :: TreePos t a -> Maybe (TreePos Full a)
-nextTree loc = case after loc of
-  t:ts -> Just loc { content = Full t, after = ts }
-  [] -> Nothing
-
-parent :: TreePos t a -> Maybe (TreePos Full a)
-parent loc = case parents loc of
-  (ls,a,rs) : ps -> Just Loc
-    { content = Full a
-    , before = ls
-    , after = rs
-    , parents = ps
-    }
-  [] -> Nothing
-
-root :: TreePos Full a -> TreePos Full a
-root loc = maybe loc root (parent loc)
-
-
-tree :: TreePos Full a -> Box a
-tree = unFull . content
