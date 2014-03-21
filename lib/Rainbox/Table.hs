1437ec88b68e925df5de40de9d01eeea0dc9136d
Omari Norman <omari@smileystation.com>
2014-03-21 12:49:06 -0400

    WIP

--- lib/Rainbox/Table.hs
+++ lib/Rainbox/Table.hs
@@ -5,47 +5,38 @@ import Data.Tuple
 import Data.Array
 import Data.List (transpose)
 import System.Console.Rainbow
-import System.Console.Rainbow.Types
-import qualified Data.Text as X
-
-data Carton = Carton
-  { cartonText :: X.Text
-  , cartonFormat :: TextSpec -> TextSpec -> TextSpec
-  -- ^ This function will be applied to the TextSpec for the Record and
-  -- the TextSpec for the column (in that order) to get the final
-  -- TextSpec for the text in the Carton.
-  }
-
-instance Show Carton where
-  show c = "carton: " ++ show (cartonText c)
-
-maxWidth :: MultiWidth a => a -> Int
-maxWidth = maximum . (0:) . multiWidth
 
 class MultiWidth a where
   multiWidth :: a -> [Int]
 
+maxWidth :: MultiWidth a => a -> Int
+maxWidth = maximum . (0:) . multiWidth
+
 -- | Forms the basis of a 'Cell'.  A single screen line of text
 -- within a single 'Cell'.
-newtype Bar a = Bar { unBar :: [a] }
+newtype Bar = Bar { unBar :: [Chunk] }
   deriving (Eq, Ord, Show)
 
-instance HasWidth a => HasWidth (Bar a) where
-  width = sum . map width . unBar
+barToBox :: Bar -> Box
+barToBox = chunks . unBar
 
-instance Functor Bar where
-  fmap f = Bar . map f . unBar
+barsToBox :: Background -> Align Horiz -> [Bar] -> Box
+barsToBox bk ah = catV bk ah . map barToBox
+
+instance HasWidth Bar where
+  width = sum . map width . unBar
 
 -- | A 'Cell' consists of multiple screen lines; each screen line is
 -- a 'Bar'.
-newtype Cell a = Cell { unCell :: [Bar a] }
-  deriving (Eq, Ord, Show)
+data Cell = Cell
+  { bars :: [Bar]
+  , horiz :: Align Horiz
+  , vert :: Align Vert
+  , background :: Background
+  } deriving (Eq, Show)
 
-instance Functor Cell where
-  fmap f = Cell . map (fmap f) . unCell
-
-instance HasWidth a => MultiWidth (Cell a) where
-  multiWidth = map width . unCell
+instance MultiWidth Cell where
+  multiWidth = map width . bars
 
 data Table lCol lRow col row a = Table
   { lCols :: Array col lCol
@@ -55,6 +46,17 @@ data Table lCol lRow col row a = Table
 
 instance (Ix col, Ix row) => Functor (Table lCol lRow col row) where
   fmap f t =  t { cells = fmap f . cells $ t }
+
+mapTable
+  :: (Ix col, Ix row)
+  => (lCol -> lRow -> col -> row -> a -> b)
+  -> Table lCol lRow col row a
+  -> Table lCol lRow col row b
+mapTable f (Table cs rs ls) = Table cs rs ls'
+  where
+    ls' = listArray (bounds ls) . map g . assocs $ ls
+      where
+        g ((col, row), e) = f (cs ! col) (rs ! row) col row e
 
 mapColLabels
   :: (Ix col, Ix row)
@@ -66,13 +68,31 @@ mapColLabels f (Table cs rs ls) = Table cs' rs ls
     ((colMin, rowMin), (colMax, rowMax)) = bounds ls
     cs' = listArray (colMin, colMax) es
       where
-        es = zipWith3 f (elems cs) (indices cs) rows
+        es = zipWith3 f (elems cs) (indices cs) rws
           where
-            rows = map mkRow . indices $ cs
+            rws = map mkRow . indices $ cs
               where
                 mkRow idx = zipWith3 (,,) (elems rs)
                   (indices rs)
                   (map (ls !) (range ((idx, rowMin), (idx, rowMax))))
+
+mapRowLabels
+  :: (Ix col, Ix row)
+  => (lRow -> row -> [(lCol, col, a)] -> lRow')
+  -> Table lCol lRow col row a
+  -> Table lCol lRow' col row a
+mapRowLabels f (Table cs rs ls) = Table cs rs' ls
+  where
+    ((colMin, rowMin), (colMax, rowMax)) = bounds ls
+    rs' = listArray (rowMin, rowMax) es
+      where
+        es = zipWith3 f (elems rs) (indices rs) cols
+          where
+            cols = map mkCol . indices $ rs
+              where
+                mkCol idx = zipWith3 (,,) (elems cs)
+                  (indices cs)
+                  (map (ls !) (range ((colMin, idx), (colMax, idx))))
 
 tableByRow
   :: (Ix col, Ix row)
@@ -80,12 +100,12 @@ tableByRow
   -> [lCol]
   -> [(lRow, [a])]
   -> Table lCol lRow col row a
-tableByRow (aMin, aMax) cols rows = Table cs rs ls
+tableByRow (aMin, aMax) cols rws = Table cs rs ls
   where
     cs = listArray (snd aMin, snd aMax) cols
-    rs = listArray (fst aMin, fst aMax) (map fst rows)
+    rs = listArray (fst aMin, fst aMax) (map fst rws)
     ls = listArray (swap aMin, swap aMax)
-      . concat . transpose . map snd $ rows
+      . concat . transpose . map snd $ rws
 
 tableByColumn
   :: (Ix col, Ix row)
@@ -93,260 +113,88 @@ tableByColumn
   -> [lRow]
   -> [(lCol, [a])]
   -> Table lCol lRow col row a
-tableByColumn (aMin, aMax) rows cols = Table cs rs ls
+tableByColumn (aMin, aMax) rws cols = Table cs rs ls
   where
-    rs = listArray (snd aMin, snd aMax) rows
+    rs = listArray (snd aMin, snd aMax) rws
     cs = listArray (fst aMin, fst aMax) (map fst cols)
     ls = listArray (aMin, aMax) . concat . map snd $ cols
 
-glueGrid
+boxCells
   :: (Ix col, Ix row)
-  => Array (col, row) (Cell Chunk, Background)
+  => Array (col, row) Cell
   -> Array (col, row) Box
-glueGrid = undefined
+boxCells ay = cells $ mapTable conv tbl
+  where
+    tbl = Table (labelCols getWidth ay) (labelRows getHeight ay) ay
+      where
+        getWidth _ = maximum . (0:) . concat . map multiWidth . map snd
+        getHeight _ = maximum . (0:) . map (length . bars . snd)
+    conv lCol lRow _ _ c = grow bk (Height lRow) (Width lCol) av ah bx
+      where
+        Cell bs ah av bk = c
+        bx = barsToBox bk ah bs
 
-drawGrid
+glueBoxes
   :: (Ix col, Ix row)
-  => (TextSpec -> TextSpec -> Background)
-  -> Table TextSpec TextSpec col row (Cell Carton)
-  -> Array (col, row) (Cell Chunk, Background)
-drawGrid = undefined
-
-{-
-  ( Bar(..)
-  , Cell(..)
-  , Record(..)
-  , Records
-  , unRecords
-  , Column(..)
-  , MultiWidth(..)
-  , maxWidth
-  , Columns
-  , unColumns
-  , Crate(..)
-  , toRecords
-  , toColumns
-  , justifyRecords
-  , Carton(..)
-  , opaque
-  , clear
-  , mappendRecord
-  , mappendColumn
-  , formatRecords
-  , formatTableTextSpec
-  , fancyTable
-  ) where
-
-
-import Data.List (transpose)
-import Rainbox
-import qualified Rainbox as B
-import Data.Monoid
-import qualified Data.Text as X
-import System.Console.Rainbow
-import System.Console.Rainbow.Types
-
--- # Tables
-
--- | A 'Record' consists of multple 'Cell' which appear on screen
--- from left to right.
-newtype Record a = Record { unRecord :: [Cell a] }
-  deriving (Eq, Ord, Show)
-
-instance Functor Record where
-  fmap f = Record . map (fmap f) . unRecord
-
--- | Several 'Record' grouped together.  For a 'Records'  to be
--- valid, every 'Record' must contain an equal number of 'Cell'.  A
--- valid 'Records' might not appear justified, because the cells
--- might not be of uniform width.
-newtype Records a = Records { unRecords :: [Record a] }
-  deriving (Eq, Ord, Show)
-
-instance Functor Records where
-  fmap f = Records . map (fmap f) . unRecords
-
--- | Several 'Cell' that are in one tabular column.
-newtype Column a = Column { unColumn :: [Cell a] }
-  deriving (Eq, Ord, Show)
-
-instance Functor Column where
-  fmap f = Column . map (fmap f) . unColumn
-
-instance HasWidth a => MultiWidth (Column a) where
-  multiWidth = concat . map multiWidth . unColumn
-
--- | Something that has multiple possible widths at different
--- points.
-
--- | Several 'Column' grouped together.  For a 'Columns' to be
--- valid, each 'Column' must have an equal number of 'Cell'.
-newtype Columns a = Columns { unColumns :: [Column a] }
-  deriving (Eq, Ord, Show)
-
-instance Functor Columns where
-  fmap f = Columns . map (fmap f) . unColumns
-
-data Crate = Crate
-  { crateText :: X.Text
-  , crateFormat :: TextSpec -> TextSpec
-  }
-
-instance Show Crate where
-  show = X.unpack . crateText
-
-toColumns :: Records a -> Columns a
-toColumns = Columns . map Column . transpose
-  . map unRecord . unRecords
-
-toRecords :: Columns a -> Records a
-toRecords = Records . map Record . transpose
-  . map unColumn . unColumns
-
-columnWidths :: HasWidth a => Columns a -> [Int]
-columnWidths = map maxWidth . unColumns
-
-
--- | Equalize the cells in each Record and justify them.
-justifyRecords
-  :: [Align Horiz]
-  -- ^ Specifies the alignment for each column in the table.  This
-  -- list can be infinite.  If the table has more columns than are
-  -- in this list, the extra columns will be formatted with a 'left'
-  -- justification.
-
-  -> [Record Chunk]
-  -- ^ Each input Record.
-
-  -> [[Background -> Box]]
-justifyRecords as rs = map justify' . unRecords $ equalLengthRows
-  where
-    equalLengthRows = equalizeRecordLengths rs
-
-    justify' = zipWith3 makeCell aligns widths . unRecord
-      where
-        aligns = as ++ repeat B.left
-        widths = columnWidths . toColumns $ equalLengthRows
-
-
--- | Takes list of input Record and returns a list of Record where
--- each Record has the same number of cells.
-equalizeRecordLengths :: [Record a] -> Records a
-equalizeRecordLengths rs = Records $ map equalize rs
-  where
-    equalize (Record rec) = Record . take maxLen
-      $ rec ++ repeat (Cell [])
-    maxLen = maximum . (0:) . map (length . unRecord) $ rs
-
--- | Creates a cell.
-makeCell
-  :: Align Horiz
-  -> Int
-  -- ^ Padded final width of cell
-  -> Cell Chunk
-  -> Background
+  => Array (col, row) Box
   -> Box
-makeCell align wdth cell bk
-  = growH bk wdth align
-  . B.catV bk align
-  . map B.chunks
-  . map unBar
-  . unCell
-  $ cell
+glueBoxes
+  = catH defaultBackground top
+  . map (catV defaultBackground left)
+  . columns
 
--- | Format the rows and columns of a table with TextSpec.
-formatTableTextSpec
-  :: [(Align Horiz, TextSpec)]
-  -- ^ One element in the list for each column in the table.  If
-  -- there are extra columns, they are formatted with the default
-  -- 'TextSpec' and with left alignment.
-
-  -> [(TextSpec, Record Crate)]
-  -- ^ Each row of the table is represented with a pair.  The
-  -- 'TextSpec' for each Record is combined with the 'TextSpec' for
-  -- each respective column to format necessary padding.
-
-  -> [[Box]]
-formatTableTextSpec formats rws = map toBoxes rws
-  where
-    fmts = formats ++ repeat (B.left, mempty)
-    toBoxes (tsRec, Record cs) = zipWith mkBox fmts cs
-      where
-        mkBox (align, tsCol) crate = boxFromCrates tsRec align tsCol crate
-
-boxFromCrates
-  :: TextSpec
-  -- ^ TextSpec for the 'Record'
-  -> Align Horiz
-  -- ^ Alignment for the column
-  -> TextSpec
-  -- ^ TextSpec for the 'Column'
-  -> Cell Crate
+grid
+  :: (Ix col, Ix row)
+  => Array (col, row) Cell
   -> Box
-boxFromCrates tsRec ah tsCol cell
-  = B.catV bk ah
-  . map B.chunks
-  . map barToChunks
-  . unCell
-  $ cell
+grid = glueBoxes . boxCells
+
+columns
+  :: (Ix col, Ix row)
+  => Array (col, row) a
+  -> [[a]]
+columns ay = map getCol $ range (minCol, maxCol)
   where
-    barToChunks = map crateToChunk . unBar
-    crateToChunk c = Chunk (crateFormat c tsCol) (crateText c)
-    bk = backgroundFromTextSpec $ tsCol <> tsRec
+    ((minCol, minRow), (maxCol, maxRow)) = bounds ay
+    ixsRows = range (minRow, maxRow)
+    getCol ixCol = map (\rw -> ay ! (ixCol, rw)) ixsRows
 
---
--- # Fancy table
---
-
--- | Use only the TextSpec given in the Chunk.  Ignore the TextSpec
--- from the Column and from the Record.
-opaque :: Chunk -> Carton
-opaque c = Carton (text c) (\_ _ -> textSpec c)
-
--- | To get the final TextSpec, start with the TextSpec for the
--- 'Column', 'mappend' the TextSpec for the 'Record', and then
--- 'mappend' the TextSpec for the 'Chunk' given here.
-clear :: Chunk -> Carton
-clear c = Carton (text c) (\col rec -> col <> rec <> textSpec c)
-
--- | To get the final TextSpec, start with the TextSpec for the
--- 'Record' and then 'mappend' the TextSpec for the 'Chunk' given
--- here.
-mappendRecord :: Chunk -> Carton
-mappendRecord c = Carton (text c) (\_ rec -> rec <> textSpec c)
-
--- | To get the final TextSpec, start with the TextSpec for the
--- 'Column' and then 'mappend' the TextSpec for the 'Chunk' given
--- here.
-mappendColumn :: Chunk -> Carton
-mappendColumn c = Carton (text c) (\col _ -> col <> textSpec c)
-
-formatRecords
-  :: [(TextSpec, Record Carton)]
-  -- ^ Each record in the table.  The TextSpec is the default
-  -- formatting for the row.  It is combined with the Carton and,
-  -- later, with column-specific formatting.
-  -> [(TextSpec, Record Crate)]
-  -- ^ Suitable for use with formatTableTextSpec.
-formatRecords = map f
+rows
+  :: (Ix col, Ix row)
+  => Array (col, row) a
+  -> [[a]]
+rows ay = map getRow $ range (minRow, maxRow)
   where
-    f (ts, rc) = (ts, fmap (cartonToCrate ts) rc)
+    ((minCol, minRow), (maxCol, maxRow)) = bounds ay
+    ixsCols = range (minCol, maxCol)
+    getRow ixRow = map (\cl -> ay ! (cl, ixRow)) ixsCols
 
--- | With information about the TextSpec for a particular Record,
--- convert a Carton to a Crate.
-cartonToCrate
-  :: TextSpec
-  -> Carton
-  -> Crate
-cartonToCrate tsRec ctn =
-  Crate (cartonText ctn) (cartonFormat ctn tsRec)
-
-fancyTable
-  :: [(Align Horiz, TextSpec)]
-  -> [(TextSpec, Record Carton)]
-  -> [[Box]]
-fancyTable cols recs = formatTableTextSpec cols cartons
+labelCols
+  :: (Ix col, Ix row)
+  => (col -> [(row, a)] -> lCol)
+  -> Array (col, row) a
+  -> Array col lCol
+labelCols f a = listArray (minCol, maxCol) es
   where
-    cartons = formatRecords recs
+    ((minCol, minRow), (maxCol, maxRow)) = bounds a
+    es = zipWith f ixsCols . map mkRow $ ixsCols
+      where
+        ixsCols = range (minCol, maxCol)
+        mkRow col = zip ixsRows (map (\rw -> a ! (col, rw)) ixsRows)
+          where
+            ixsRows = range (minRow, maxRow)
 
--}
+labelRows
+  :: (Ix col, Ix row)
+  => (row -> [(col, a)] -> lRow)
+  -> Array (col, row) a
+  -> Array row lRow
+labelRows f a = listArray (minRow, maxRow) es
+  where
+    ((minCol, minRow), (maxCol, maxRow)) = bounds a
+    es = zipWith f ixsRows . map mkCol $ ixsRows
+      where
+        ixsRows = range (minRow, maxRow)
+        mkCol row = zip ixsCols (map (\cl -> a ! (cl, row)) ixsCols)
+          where
+            ixsCols = range (minCol, maxCol)
