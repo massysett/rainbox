82541f8eed577a57e8999913b7a08db6ee27fc7f
Omari Norman <omari@smileystation.com>
2014-03-23 11:57:42 -0400

    add sunlight

---
+++ sunlight-test.hs
@@ -0,0 +1,14 @@
+module Main where
+
+import Test.Sunlight
+
+inputs = TestInputs
+  { tiDescription = Nothing
+  , tiCabal = "cabal"
+  , tiLowest = ("7.4.1", "ghc-7.4.1", "ghc-pkg-7.4.1")
+  , tiDefault = [ ("7.4.1", "ghc-7.4.1", "ghc-pkg-7.4.1")
+                , ("7.6.3", "ghc-7.6.3", "ghc-pkg-7.6.3") ]
+  , tiTest = []
+  }
+
+main = runTests inputs
