10e783259e4a6186b3877d234f98e85de7df2475
Omari Norman <omari@smileystation.com>
2015-02-17 07:49:11 -0500

    update Cartel

--- genCabal.hs
+++ genCabal.hs
@@ -1,92 +1,80 @@
 -- Generates a Cabal file using the Cartel package.
 --
--- Written for Cartel version 0.10.0.2.
+-- Written for Cartel version 0.14.2.0
 
-import qualified Cartel as A
+import Cartel
 
 -- Package version
-version :: A.Version
-version = A.Version [0,8,0,0]
+pkgVersion :: [Word]
+pkgVersion = [0,10,0,0]
 
 -- Dependencies
 
-base :: A.Package
-base = A.closedOpen "base" [4,5,0,0] [4,8,0,0]
+base :: Package
+base = closedOpen "base" [4,5,0,0] [4,8,0,0]
 
-rainbow :: A.Package
-rainbow = A.nextBreaking "rainbow" [0,20]
+rainbow :: Package
+rainbow = nextBreaking "rainbow" [0,20]
 
-rainbow_tests :: A.Package
-rainbow_tests = A.nextBreaking "rainbow-tests" [0,20]
+terminfo :: Package
+terminfo = closedOpen "terminfo" [0,3,2] [0,5,0,0]
 
-terminfo :: A.Package
-terminfo = A.closedOpen "terminfo" [0,3,2] [0,5,0,0]
+text :: Package
+text = closedOpen "text" [0,11,3,1] [1,3,0,0]
 
-text :: A.Package
-text = A.closedOpen "text" [0,11,3,1] [1,3,0,0]
+transformers :: Package
+transformers = closedOpen "transformers" [0,3,0,0] [0,5,0,0]
 
-transformers :: A.Package
-transformers = A.closedOpen "transformers" [0,3,0,0] [0,5,0,0]
+array :: Package
+array = closedOpen "array" [0,4,0,0] [0,6,0,0]
 
-array :: A.Package
-array = A.closedOpen "array" [0,4,0,0] [0,6,0,0]
+tasty :: Package
+tasty = closedOpen "tasty" [0,10,1] [0,11]
 
-tasty :: A.Package
-tasty = A.closedOpen "tasty" [0,10,1] [0,11]
+tasty_quickcheck :: Package
+tasty_quickcheck = closedOpen "tasty-quickcheck" [0,8,1] [0,9]
 
-tasty_quickcheck :: A.Package
-tasty_quickcheck = A.closedOpen "tasty-quickcheck" [0,8,1] [0,9]
+quickcheck :: Package
+quickcheck = closedOpen "QuickCheck" [2,7,5] [2,8]
 
-quickcheck :: A.Package
-quickcheck = A.closedOpen "QuickCheck" [2,7,5] [2,8]
+random :: Package
+random = closedOpen "random" [1,0,0,0] [1,2]
 
-random :: A.Package
-random = A.closedOpen "random" [1,0,0,0] [1,2]
-
-properties :: A.Properties
-properties = A.empty
-  { A.prName = "rainbox"
-  , A.prVersion = version
-  , A.prLicense = A.BSD3
-  , A.prLicenseFile = "LICENSE"
-  , A.prCopyright = "Copyright 2014 Omari Norman"
-  , A.prAuthor = "Omari Norman"
-  , A.prMaintainer = "omari@smileystation.com"
-  , A.prStability = "Experimental"
-  , A.prHomepage = "http://www.github.com/massysett/rainbox"
-  , A.prBugReports = "http://www.github.com/massyett/rainbox/issues"
-  , A.prSynopsis = "Two-dimensional box pretty printing, with colors"
-  , A.prDescription =
+properties :: Properties
+properties = blank
+  { name = "rainbox"
+  , version = pkgVersion
+  , cabalVersion = Just (1, 14)
+  , buildType = Just simple
+  , license = Just bsd3
+  , licenseFile = "LICENSE"
+  , copyright = "Copyright 2014-2015 Omari Norman"
+  , author = "Omari Norman"
+  , maintainer = "omari@smileystation.com"
+  , stability = "Experimental"
+  , homepage = "http://www.github.com/massysett/rainbox"
+  , bugReports = "http://www.github.com/massyett/rainbox/issues"
+  , synopsis = "Two-dimensional box pretty printing, with colors"
+  , description =
     [ "Prints boxes in two dimensions, with colors.  Boxes are"
     , "automatically padded with necessary whitespace."
     , ""
     , "For more information, please see the Haddock documentation and"
     , ""
-    , "<http://www.github.com/massysett/rainbox"
+    , "<http://www.github.com/massysett/rainbox>"
     ]
-  , A.prCategory = "Text"
-  , A.prExtraSourceFiles =
+  , category = "Text"
+  , extraSourceFiles =
     [ "README.md"
-    , "sunlight-test.hs"
-    , "minimum-versions.txt"
-    , "current-versions.txt"
     , "changelog"
     ]
   }
 
-repo :: A.Repository
-repo = A.empty
-  { A.repoVcs = A.Git
-  , A.repoKind = A.Head
-  , A.repoLocation = "git://github.com/massysett/rainbox.git"
-  , A.repoBranch = "master"
-  }
+ghcOpts :: HasBuildInfo a => a
+ghcOpts = ghcOptions ["-Wall"]
 
-ghcOptions :: A.Field a => a
-ghcOptions = A.ghcOptions ["-Wall"]
-
-libDeps :: A.Field a => a
-libDeps = A.buildDepends
+libDeps :: HasBuildInfo a => a
+libDeps = buildDepends
   [ base
   , rainbow
   , text
@@ -97,103 +85,93 @@ libDeps = A.buildDepends
 library
   :: [String]
   -- ^ Library modules
-  -> A.Library
-library ms = A.Library
-  [ A.LibExposedModules ms
-  , A.defaultLanguage A.Haskell2010
-  , ghcOptions
-  , A.hsSourceDirs ["lib"]
+  -> [LibraryField]
+library ms =
+  [ exposedModules ms
+  , haskell2010
+  , ghcOpts
+  , hsSourceDirs ["lib"]
   , libDeps
   ]
 
 visual
   :: [String]
   -- ^ Visual modules
-  -> A.TestSuite
-visual ms = A.TestSuite "rainbox-visual" $
-  [ A.TestType A.ExitcodeStdio
-  , ghcOptions
-  , A.otherModules ms
-  , A.TestMainIs "rainbox-visual.hs"
-  , A.hsSourceDirs ["test", "lib"]
-  , A.defaultLanguage A.Haskell2010
+  -> Section
+visual ms = testSuite "rainbox-visual" $
+  [ exitcodeStdio
+  , ghcOpts
+  , otherModules ms
+  , mainIs "rainbox-visual.hs"
+  , hsSourceDirs ["test", "lib"]
+  , haskell2010
   , libDeps
-  , A.buildDepends
+  , buildDepends
     [ tasty
     , tasty_quickcheck
     , quickcheck
     , random
-    , rainbow_tests
     ]
   ]
 
 mosaic
-  :: [String]
+  :: FlagName
+  -- ^ mosaic flag
+  -> [String]
   -- ^ Mosaic modules
-  -> A.Executable
-mosaic ms = A.Executable "rainbox-mosaic"
-  [ A.ExeMainIs "rainbox-mosaic.hs"
-  , A.cif (A.flag "mosaic")
-    [ ghcOptions
-    , A.otherModules ms
-    , A.hsSourceDirs ["test", "lib"]
-    , A.defaultLanguage A.Haskell2010
-    , libDeps
-    , A.buildDepends
-      [ tasty
-      , tasty_quickcheck
-      , quickcheck
-      , random
-      , rainbow_tests
+  -> Section
+mosaic mosaic ms = executable "rainbox-mosaic"
+  [ mainIs "rainbox-mosaic.hs"
+  , condBlock (flag mosaic)
+    ( ghcOpts
+    , [ otherModules ms
+      , hsSourceDirs ["test", "lib"]
+      , haskell2010
+      , libDeps
+      , buildDepends
+        [ tasty
+        , tasty_quickcheck
+        , quickcheck
+        , random
+        ]
       ]
-    ]
-    [ A.buildable False]
+    )
+    [ buildable False]
   ]
 
 mainTest
   :: [String]
   -- ^ Test modules
-  -> A.TestSuite
-mainTest ms = A.TestSuite "rainbox-test"
-  [ ghcOptions
-  , A.TestType A.ExitcodeStdio
-  , A.hsSourceDirs ["test", "lib"]
-  , A.defaultLanguage A.Haskell2010
+  -> Section
+mainTest ms = testSuite "rainbox-test"
+  [ ghcOpts
+  , exitcodeStdio
+  , hsSourceDirs ["test", "lib"]
+  , haskell2010
   , libDeps
-  , A.TestMainIs "rainbox-test.hs"
-  , A.buildDepends
+  , mainIs "rainbox-test.hs"
+  , buildDepends
     [ tasty
     , tasty_quickcheck
     , quickcheck
-    , rainbow_tests
     ]
   ]
 
-flagVisual :: A.Flag
-flagVisual = A.empty
-  { A.flName = "mosaic"
-  , A.flDescription = "Build the rainbox-mosaic executable"
-  , A.flDefault = False
-  , A.flManual = True
-  }
-
-cabal
-  :: [String]
-  -- ^ Library modules
-  -> [String]
-  -- ^ Test modules
-  -> A.Cabal
-cabal ms ts = A.empty
-  { A.cProperties = properties
-  , A.cRepositories = [repo]
-  , A.cLibrary = Just $ library ms
-  , A.cExecutables = [mosaic $ ms ++ ts]
-  , A.cTestSuites = [ mainTest $ ms ++ ts, visual $ ms ++ ts ]
-  , A.cFlags = [ flagVisual ]
-  }
-
 main :: IO ()
-main = do
-  ms <- A.modules "lib"
-  ts <- A.modules "test"
-  A.render "genCabal.hs" $ cabal ms ts
+main = defaultMain $ do
+  ms <- modules "lib"
+  ts <- modules "test"
+  fl <- makeFlag "mosaic" $ FlagOpts
+    { flagDescription = "Build the rainbox-mosaic executable"
+    , flagDefault = False
+    , flagManual = True
+    }
+  return
+    ( properties
+    , library ms
+    , [ visual (ms ++ ts)
+      , mosaic fl (ms ++ ts)
+      , mainTest (ms ++ ts)
+      , githubHead "massysett" "rainbox"
+      ]
+    )
