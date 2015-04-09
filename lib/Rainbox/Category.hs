120a07e2558539e6736ebe1f0f0139a61801b162
Omari Norman <omari@smileystation.com>
2015-04-09 15:57:29 -0400

    WIP

--- lib/Rainbox/Category.hs
+++ lib/Rainbox/Category.hs
@@ -1,14 +1,19 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE Trustworthy #-}
+{-# LANGUAGE OverloadedLists #-}
 module Rainbox.Category where
 
 import Rainbow
+import Control.Monad (join)
+import Data.Monoid
 import Rainbow.Types (Chunk(..))
-import Data.Sequence (Seq, ViewL(..), viewl) --, (<|), (|>))
+import Data.Sequence (Seq, ViewL(..), viewl, (|>))
 import qualified Data.Foldable as F
 import qualified Data.Sequence as Seq
 import qualified Data.Text as X
+import GHC.Exts (IsList(..))
 
 -- | Alignment.
 data Align a = Center | NonCenter a
@@ -49,6 +54,23 @@ rodsFromCore :: Radiant -> Core -> Seq Rod
 rodsFromCore rd (Core ei) = case ei of
   Left ck -> Seq.singleton . Rod . Right $ ck
   Right (Height h, Width w) -> Seq.replicate h . Rod . Left $ (w, rd)
+
+rodsLength :: Seq Rod -> Int
+rodsLength = F.sum . fmap toLen
+  where
+    toLen (Rod ei) = case ei of
+      Left (i, _) -> i
+      Right (Chunk _ xs) -> F.sum . fmap X.length $ xs
+
+chunksFromRods :: Seq (Seq Rod) -> Seq (Seq Chunk)
+chunksFromRods = fmap (|> "\n") . fmap (fmap chunkFromRod)
+  where
+    chunkFromRod (Rod ei) = case ei of
+      Left (i, r) -> (chunkFromText . X.replicate i $ " ") <> back r
+      Right c -> c
+
+render :: Box a => a -> [Chunk]
+render = F.toList . join . chunksFromRods . makeBox
 
 instance HasWidth Core where
   width (Core ei) = case ei of
@@ -109,7 +131,10 @@ instance UpDown BoxHP where
     Center -> snd . split $ either height height ei
 
 newtype BoxV = BoxV (Seq BoxVP)
-  deriving Monoid
+
+instance Monoid BoxV where
+  mempty = BoxV mempty
+  mappend (BoxV x) (BoxV y) = BoxV $ x <> y
 
 instance LeftRight BoxV where
   port (BoxV sq) = F.foldl' max 0 . fmap port $ sq
@@ -122,7 +147,10 @@ instance HasHeight BoxV where
   height (BoxV sq) = F.sum . fmap height $ sq
 
 newtype BoxH = BoxH (Seq BoxHP)
-  deriving Monoid
+
+instance Monoid BoxH where
+  mempty = BoxH mempty
+  mappend (BoxH x) (BoxH y) = BoxH $ x <> y
 
 instance Box BoxH where
   makeBox bx@(BoxH sqnce) = mergeHoriz $ fmap equalize sqnce
@@ -155,31 +183,49 @@ instance HasWidth BoxH where
 instance Box BoxV where
   makeBox bx@(BoxV sqnce) = mergeVert $ fmap equalize sqnce
     where
-      maxLeft = port bx
-      maxRight = starboard bx
-      h = height bx
       mergeVert = F.foldl' (<>) Seq.empty
-      equalize bvp@(BoxVP _ rd ei) = addLeftRight this
+      equalize (BoxVP a rd ei) = fmap addLeftRight this
         where
           this = either makeBox (fmap Seq.singleton $ rodsFromCore rd) ei
-          addLeftRight sqn = undefined
+          addLeftRight lin = padder lenLft <> lin <> padder lenRgt
+            where
+              lenLin = rodsLength lin
+              lenLft = case a of
+                Center -> port bx - (fst . split $ lenLin)
+                NonCenter ALeft -> 0
+                NonCenter ARight -> port bx - lenLin
+              lenRgt = case a of
+                Center -> starboard bx - (snd . split $ lenLin)
+                NonCenter ALeft -> starboard bx - lenLin
+                NonCenter ARight -> 0
+              padder len
+                | len < 1 = Seq.empty
+                | otherwise = Seq.singleton . Rod . Left
+                      $ (len, rd)
 
 class Alignment a where
   type BuiltBox a
   type Opposite a
   buildBox :: a -> Radiant -> Either (Opposite a) Core -> BuiltBox a
+  segment :: Radiant -> Int -> BuiltBox a
 
 instance Alignment (Align Vert) where
   type BuiltBox (Align Vert) = BoxH
   type Opposite (Align Vert) = BoxV
   buildBox a r ei = BoxH . Seq.singleton $
     BoxHP a r ei
+  segment r i = BoxH . Seq.singleton $
+    BoxHP (NonCenter ATop) r (Right . Core . Right $
+      (Height 0, Width i))
 
 instance Alignment (Align Horiz) where
   type BuiltBox (Align Horiz) = BoxV
   type Opposite (Align Horiz) = BoxH
   buildBox a r ei = BoxV . Seq.singleton $
     BoxVP a r ei
+  segment r i = BoxV . Seq.singleton $
+    BoxVP (NonCenter ALeft) r (Right . Core . Right $
+      (Height i, Width 0))
 
 fromChunk
   :: Alignment a
@@ -205,6 +251,61 @@ convert
   -> Opposite a
   -> BuiltBox a
 convert a r o = buildBox a r (Left o)
+
+newtype CellRow = Row (Seq Chunk)
+  deriving (Eq, Ord, Show)
+
+instance IsList CellRow where
+  type Item CellRow = Chunk
+  fromList = Row . Seq.fromList
+  toList (Row sq) = F.toList sq
+
+newtype Cell = Cell (Seq CellRow)
+  deriving (Eq, Ord, Show)
+
+instance IsList Cell where
+  type Item Cell = CellRow
+  fromList = Cell . Seq.fromList
+  toList (Cell sq) = F.toList sq
+
+newtype RowCol a = RowCol (Seq (a, Radiant, Cell))
+  deriving (Eq, Ord, Show)
+
+instance IsList (RowCol a) where
+  type Item (RowCol a) = (a, Radiant, Cell)
+  fromList = RowCol . Seq.fromList
+  toList (RowCol sq) = F.toList sq
+
+type Row = RowCol
+type Column = RowCol
+
+newtype RowsCols a = RowsCols (Seq (RowCol a))
+  deriving (Eq, Ord, Show)
+
+instance IsList (RowsCols a) where
+  type Item (RowsCols a) = RowCol a
+  fromList = RowsCols . Seq.fromList
+  toList (RowsCols sq) = F.toList sq
+
+type Rows = RowsCols (Align Vert)
+type Columns = RowsCols (Align Horiz)
+
+table
+  :: Alignment a
+  => RowsCols a
+  -> BuiltBox a
+table = undefined
+
+tableByRows
+  :: Rows
+  -> BoxH
+tableByRows = table
+
+tableByColumns
+  :: Columns
+  -> BoxV
+tableByColumns = table
+
 
 -- | Split a number into two parts, so that the sum of the two parts
 -- is equal to the original number.
